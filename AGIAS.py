3c21ca554bbfe2ad471aedd179a106e857fd9b10
Vlad Constantinescu <V.Constantinescu@student.tudelft.nl>
2023-05-07 23:44:49 +0200

    Add experiment week 2

--- AGIAS.py
+++ AGIAS.py
@@ -49,7 +49,8 @@ class Node:
         distinct alert signatures: {len(self._alert_signatures)})" 
 
 class Edge:
-    def __init__(self, from_node, to_node, label:dict):
+    def __init__(self, id, from_node, to_node, label:dict):
+        self._id = id
         self._from = from_node
         self._to = to_node
         self._label = label
@@ -67,14 +68,37 @@ class Graph:
         self._source_states = source_states
         self._sink_states = sink_states
 
-    def exportAsDot(self):
+    def exportAsDot(self, file_name):
         graph = pydot.Dot(graph_type='digraph')
         for node in self._nodes:
             for edge in node._outgoing_edges:
                 graph.add_edge(pydot.Edge(edge._from._label, edge._to._label, label=str(edge._label)))
                 
-        graph.write_png("out.png")
+        graph.write_png(file_name+".png")
 
+    def computeNumberOfPathsFromSourcesToSink(self) -> int:
+        self.computeNumberOfPathsFromSourcesToSinkIgnore([])
+
+    def computeNumberOfPathsFromSourcesToSinkIgnore(self, nodes_to_ignore):
+        nr_paths = 0
+        for source in self._source_states:
+            #compute nr of paths from source to any sink
+            nr_paths += self.computeNumberOfPathsFromSource(source, nodes_to_ignore, []) 
+
+        return nr_paths
+    # DFS
+    def computeNumberOfPathsFromSource(self, current, nodes_to_ignore, visited) -> int:
+        if current._id in nodes_to_ignore:
+            return 0
+        if current._id in [sink._id for sink in self._sink_states]:
+            return 1
+        paths = 0
+        for edge in current._outgoing_edges:
+            if edge._id in visited:
+                continue
+            paths += self.computeNumberOfPathsFromSource(edge._to, nodes_to_ignore, visited + [edge._id])
+
+        return paths
 
     def __str__(self):
         return f"Graph(nr nodes:{len(self._nodes)}, \
@@ -100,24 +124,27 @@ class DotParser:
 
         # check for duplicates
         edgeExistance = [[False for _ in range(len(node_map))] for _ in range(len(node_map))]
-
+        edgeIndex = 0
         for e in graph.edges:
             if(edgeExistance[node_map[e[0]]._id][node_map[e[1]]._id]):
                 continue
 
             edgeExistance[node_map[e[0]]._id][node_map[e[1]]._id] = True
+
             for _x in graph[e[0]][e[1]]:
                 ed = graph[e[0]][e[1]][_x]
                 edge = None
                 if 'label' in ed:
                     labelData = self._labelDataParser.parse_data(ed['label'])
-                    edge = Edge(node_map[e[0]], node_map[e[1]], labelData)
+                    edge = Edge(edgeIndex, node_map[e[0]], node_map[e[1]], labelData)
                 else:
                     node_map[e[1]].set_is_sink(True)                        # No edge data to him, so it is a sink
-                    edge = Edge(node_map[e[0]], node_map[e[1]], {})
+                    edge = Edge(edgeIndex, node_map[e[0]], node_map[e[1]], {})
                     
                 node_map[e[0]].add_outgoing_edge(edge)
                 node_map[e[1]].add_ingoing_edge(edge)
+                # print(edgeIndex)
+                edgeIndex += 1
                 
         nodes = [node_map[_x] for _x in node_map]
         source_states = [node_map[_x] for _x in node_map if len(node_map[_x]._ingoing_edges)==0]
@@ -125,7 +152,7 @@ class DotParser:
         
         return Graph(nodes, source_states, sink_states)
 
-#MAIN
+# MAIN
 
 # parser = DotParser(SAGEDataLabelParser())
 # graph = parser.parseFromFile("AG/ag1.dot")
