ed1a015104511bc2a7708231405544f5f9e26de3
Vlad Constantinescu <V.Constantinescu@student.tudelft.nl>
2023-05-24 17:07:12 +0200

    Run experiment for 2017 dataset

--- AGIAS.py
+++ AGIAS.py
@@ -26,13 +26,14 @@ class SAGEDataLabelParser(DataLabelParserInterface):
         
 
 class Node:
-    def __init__(self, id, label, alert_signatures):
+    def __init__(self, id, label, alert_signatures, attack_type):
         self._id = id
         self._label = label
         self._alert_signatures = alert_signatures
         self._outgoing_edges = []
         self._ingoing_edges = []
         self._is_sink = False
+        self._type = attack_type
 
     def set_is_sink(self, isSink):
         self._is_sink = isSink
@@ -63,10 +64,12 @@ class Edge:
 
 
 class Graph:
-    def __init__(self, nodes, source_states, sink_states):
+    def __init__(self, nodes, source_states, sink_states, teams):
         self._nodes = nodes
         self._source_states = source_states
         self._sink_states = sink_states
+        self._teams = teams
+
 
     def exportAsDot(self):
         graph = pydot.Dot(graph_type='digraph')
@@ -76,7 +79,20 @@ class Graph:
                 
         graph.write_png("out.png")
 
+    def analysePatterns(self, oldResults):
+        results = oldResults
 
+        for node in self._nodes:
+            for edge in node._outgoing_edges:
+                if node._type in results:
+                    if edge._to._type in results[node._type]:
+                        results[node._type][edge._to._type] += 1
+                    else:
+                        results[node._type][edge._to._type] = 1
+                else:
+                    results[node._type] = {edge._to._type: 1}
+        return results
+    
     def __str__(self):
         return f"Graph(nr nodes:{len(self._nodes)}, \
         nr source states:{len(self._source_states)}, \
@@ -90,13 +106,15 @@ class DotParser:
         """Parse a .dot file"""
         graph = nx.nx_pydot.read_dot(path)
         node_labels = nx.get_node_attributes(graph, 'tooltip')
+        teams = []
 
-        node_map = {label:Node(index,label,node_labels[label].split('\n')) for index,label in enumerate(node_labels)}
+        # TO DO :we miss the protocol and targets
+        node_map = {label:Node(index,label,node_labels[label].split('\n'), label.split("\\")[0].split("\n")[0]) for index,label in enumerate(node_labels)}
         current_node_index = len(node_labels)
         for node in graph.nodes:                                          # Add nodes that are missing labels
             if node in node_map:
                 continue
-            node_map[node] = Node(current_node_index, node, []);
+            node_map[node] = Node(current_node_index, node, [], node.split("\n")[1])
             current_node_index += 1
 
         # check for duplicates
@@ -111,8 +129,10 @@ class DotParser:
                 ed = graph[e[0]][e[1]][_x]
                 edge = None
                 team = "black"
-                if 'color' in ed:
+                if 'color' in ed:                                           # different colors determine different attacker teams
                     team = ed['color']
+                if team != "black" and team not in teams:
+                    teams.append(team)
                 if 'label' in ed:
                     labelData = self._labelDataParser.parse_data(ed['label'])
                     edge = Edge(node_map[e[0]], node_map[e[1]], labelData, team)
@@ -126,12 +146,18 @@ class DotParser:
         nodes = [node_map[_x] for _x in node_map]
         source_states = [node_map[_x] for _x in node_map if len(node_map[_x]._ingoing_edges)==0]
         sink_states = [node_map[_x] for _x in node_map if len(node_map[_x]._outgoing_edges)==0]
-        
-        return Graph(nodes, source_states, sink_states)
 
+        return Graph(nodes, source_states, sink_states, teams)
 #MAIN
 
 # parser = DotParser(SAGEDataLabelParser())
 # graph = parser.parseFromFile("AG_2018/ag1.dot")
 
+# patterns = []
+# patterns = graph.analysePatterns(patterns)
 # graph.exportAsDot()
+# # print(patterns)
+# for pattern in patterns:
+#     print(pattern)
+#     print(patterns[pattern])
+#     print("________")
