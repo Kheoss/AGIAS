d052095c36882310ba95a5a97a610d4be4f88573
Vlad Constantinescu <V.Constantinescu@student.tudelft.nl>
2023-06-24 12:55:23 +0200

    Add ranking comparison

--- experiment.py
+++ experiment.py
@@ -52,6 +52,29 @@ Planarity of density of 30.000000000000004%
 
 """
 
+"""
+Experiment 2
+2018:
+
+Total tests:2850
+Total number of discrepancies: 97.0, aprox 3%
+
+Manual analysis:
+63 fits the expert's opinion and 34 are consideret too subjective to be 
+
+
+2017:
+Total tests:14028
+Total number of discrepancies: 913 aprox 6%
+
+
+Qualitative analysis ( ACCEPTED | QUESTIONABLE ):
+CPTC-2017: 7 | 3 ( 0.013 + 0.28 + 0.178 ) AVG: 0.15
+
+
+"""
+
+
 import AGIAS
 import os
 import pathlib
@@ -59,11 +82,19 @@ import shutil
 import matplotlib.pyplot as plt
 import numpy as np
 import csv
+from scipy import stats
 
 #Parser setup
 parser = AGIAS.DotParser(AGIAS.SAGEDataLabelParser())
 non_zero_path_graphs = 0
 total_graphs = 0
+
+class Pair:
+    def __init__(self, first, second):
+        self._first = first
+        self._second = second
+    def __str__(self):
+        return f"Pair({self._first}, {self._second})"
 
 class Touple:
     def __init__(self, from_node, to_node, value):
@@ -76,57 +107,77 @@ class Touple:
 
 patterns = []
 #load patterns
-file1 = open('patterns_2017.csv', 'r')
-lines = file1.readlines()
-count = 0
-for line in lines:
-    if count > 0:
-        parts = line.split(',')
-        patterns.append(Touple(parts[0], parts[1], float(parts[2])))
-    count += 1
 
+# file1 = open('patterns_2017.csv', 'r')
+# lines = file1.readlines()
+# count = 0
+# for line in lines:
+#     if count > 0:
+#         parts = line.split(',')
+#         patterns.append(Touple(parts[0], parts[1], float(parts[2])))
+#     count += 1
 
-with open(f'pairwise.csv', 'w', newline='') as csvfile:    
-    explainability_baseline = []
-    explainability = []
-    for file in os.listdir('AG_2017/ExperimentAGs'):
-        if file.endswith('.dot'):
-            total_graphs += 1
-            graph = parser.parseFromFile("AG_2017/ExperimentAGs/" + file)
-            chunks = graph.splitIntoCognitiveChunks(patterns)
-            interdependency = graph.calculateInterdependencyPerChunkWithClosenessCentrality(chunks)
-            radial_spread = graph.calculateRadialSpread()
-            monotonicity = graph.calculateMonotonicity()
-            chunks_number = len(set(chunks))
-            
-            
-            print(f"_______________________________Graph:{total_graphs}_______________________________")
-            planarity, relative_planarity = graph.calculatePlanarity()
+explainability_baseline = []
+explainability = []
 
-            # calculate explainability 
-            base_explainability = (1/chunks_number + (1-interdependency))/2
-            my_explainability = (1/chunks_number + (1-interdependency) + planarity)/3
-            
-            explainability_baseline.append(base_explainability)
-            explainability.append(my_explainability)
+with open(f'pairwise_2017.csv', 'r') as file1:
+    lines = file1.readlines()
+    count = 0
+    for line in lines:
+        if count > 0:
+            parts = line.split(',')
+            explainability_baseline.append(Pair(count, float(parts[1])/2))
+            explainability.append(Pair(count, float(parts[0])))
+            # print(f"read: {float(parts[0])} : {float(parts[1])/2}")
+        count += 1
 
-            graph.exportAsDot(str(total_graphs))
-            graph.exportAsDotPerChunks(str(total_graphs), chunks)
+    # Sort the attack graphs
+    explainability.sort(key = lambda x : x._second, reverse=True)
+    explainability_baseline.sort(key = lambda x : x._second, reverse=True)
+    
+    # Create rankings
+    ranking_our_aux = [x._first for x in explainability]
+    ranking_base_aux = [x._first for x in explainability_baseline]
 
+    ranking_our = [0 for _ in explainability]
+    ranking_base = [0 for _ in explainability]
+
+    for index, val in enumerate(ranking_our_aux):
+        ranking_our[val-1] = index
+    for index, val in enumerate(ranking_base_aux):
+        ranking_base[val-1] = index
+
+    # Compute the tau-b value:
+    res = stats.kendalltau(ranking_our, ranking_base, variant="b")
+
+    print(f"Result: {res}")
+
+    print("Done")
+
+    # Sampling and debugging the discordant pairs
+    """
     # start pariwise comparison
     print("Start pairwise comparison")
     discrepancies = 0
+    total_tests = 0
     for i in range(len(explainability)):
-        for j in range(len(explainability)):
+        for j in range(i, len(explainability)):
             if i == j:
                 continue
-            if explainability[i] > explainability[j] and my_explainability[i] < my_explainability[j]:
-                print(f"Discrepancy found at between graphs:{i} and {j}")
+            total_tests += 1
+            if explainability[i]._second > explainability[j]._second and explainability_baseline[i]._second < explainability_baseline[j]._second:
+                print(f"Discrepancy found at between graphs:{i} and {j}: \n baseline[{explainability_baseline[i]._second}.{explainability_baseline[j]._second}] \n new[{explainability[i]},{explainability[j]}]")
                 discrepancies += 1
+                continue
             
-            if explainability[i] < explainability[j] and my_explainability[i] > my_explainability[j]:
-                print(f"Discrepancy found at between graphs:{i} and {j}")
+            if explainability[i]._second < explainability[j]._second and explainability_baseline[i]._second > explainability_baseline[j]._second:
+                print(f"Discrepancy found at between graphs:{i} and {j}: \n baseline[{explainability_baseline[i]._second}.{explainability_baseline[j]._second}] \n new[{explainability[i]},{explainability[j]}]")
                 discrepancies += 1
+                continue
 
-
+    concordant = total_tests - discrepancies
+    print(f"Total tests:{total_tests}")
     print(f"Total number of discrepancies: {discrepancies}")
+    
+    print(f"Tau = {(concordant-discrepancies)/(concordant + discrepancies)}")
+    """
